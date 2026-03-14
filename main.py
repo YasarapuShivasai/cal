529b0c10aa29fb2c8b57fa382b4f122968a86550
yasarapushivasai2005-nani <yasarapushivasai2005@gmail.com>
2026-03-14 19:25:17 +0530

    update code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 st.title("Calculator Application")
-a=st.number_input("Enter a Num:")
-b=st.number_input("Enter b Num:")
+a=st.number_input("Enter a Num:",min_value=0)
+b=st.number_input("Enter b Num:",min_value=0)
 is_clicked=st.button("Add",type='primary',use_container_width=True)
 
 if is_clicked:
